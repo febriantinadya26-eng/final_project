66c127c2f33ed47ae92eba19935dd71812e22134
febriantinadya26-eng <febriantinadya26@gmail.com>
2026-08-24 22:32:20 +0700

    add transport hub layer

--- web-finalproject/src/main.js
+++ web-finalproject/src/main.js
@@ -1,5 +1,7 @@
 import { Map } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import TransportHub from "./data/Transport Hub.geojson?url";
+
 
 const mapElement = document.createElement('div');
 mapElement.id = 'map';
@@ -9,4 +11,25 @@ document.body.appendChild(mapElement);
 const map = new Map({
   container: 'map',
   style: 'https://demotiles.maplibre.org/globe.json', // style URL
+  center: [110.37, -7.79], // starting position [lng, lat]
+  zoom: 5 // starting zoom
+})
+
+map.on("load", () => {
+
+  map.addSource("TransportHub", {
+    type: "geojson",
+    data: TransportHub
+  });
+
+  map.addLayer({
+    id: "TransportHub-layer",
+    type: "circle",
+    source: "TransportHub",
+    paint: {
+      "circle-radius": 5,
+      "circle-color": "#FF0000"
+    }
+  });
+
 })
